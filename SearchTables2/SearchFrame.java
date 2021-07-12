3d215cdb94f5dde047b8286ffeb8b00144ee929e
apmbonifacio <60639961+apmbonifacio@users.noreply.github.com>
2021-07-13 00:58:06 +0800

    Search UI

---
+++ SearchTables2/SearchFrame.java
@@ -0,0 +1,23 @@
+package SearchTables2;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class SearchFrame extends JFrame {
+    SearchPanel searchPanel;
+    
+    public SearchFrame() {
+        searchPanel = new SearchPanel();
+        this.add(searchPanel);
+        
+        this.setSize(new Dimension(1920,1080));
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setBackground(Color.WHITE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new SearchFrame();
+    }
+}
