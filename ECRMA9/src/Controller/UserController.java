7018600d059f309b58425f72c0e6eb4a7cf85656
Dani <dani.cstech@gmail.com>
2021-07-29 22:01:19 +0800

    user controller updated

--- ECRMA9/src/Controller/UserController.java
+++ ECRMA9/src/Controller/UserController.java
@@ -5,41 +5,55 @@
  */
 package Controller;
 
+import Model.User;
+import Model.UserDatabase;
+
 /**
  *
  * @author dandani-cs
  */
 public class UserController {
-    // ADD instance of model
-    // private UserModel userDB;
-    // private int userIndex;
-    // private User current_user;
+    private UserDatabase userDB;
+    private User current_user;
     
     public UserController () {
-        // userDB = new UserModel();
-        
+        userDB = new UserDatabase("users.txt");
     }
     
-    public boolean isAuthorized() { // pass on User object (User user)
-        userIndex = getUsernames.indexOf(user.getUsername());
-        if (userIndex == -1) 
-            return false;
-                    
-        if (getPassword(userIndex) == user.getPassword()) {
-            current_user = user;
-            return true; 
-        }
-            
+    public boolean isAuthorized(String email, String password) { // pass on User object (User user)
+        User attempt = userDB.contains_credentials(email, password);
         
+        if (attempt != null) {
+            current_user = attempt;
+            return true;
+        }    
         return false;
     }
     
-    public String getUserType() {
+    public Boolean isAdmin() {
         try {
-            return current_user.getUserType();
+            return current_user.get_is_admin();
         } catch (NullPointerException e) {
             System.out.println("User is not logged in.");
             return null;
         }
+    }
+    
+    public void logout() {
+        current_user = null;
+    }
+    
+    public String signup(String email, String password, String confirm_password) {
+        if (!password.equals(confirm_password)) {
+            return "Passwords do not match";
+        }
+        
+        if (userDB.existing_email(email)) {
+            return "There is a record with this email.";
+        }
+        
+        User new_user = new User(email, password, false);
+        userDB.add_user(new_user);
+        return "Sign up successful";
     }
 }
